c5311b4d62fbb97f7a4ce4786f712a9300ba0aa6
Rebone Mogotsi <rebone.mogotsi@gmail.com>
2024-01-07 19:49:19 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,16 +5,19 @@
 */
 void times_table(void)
 {
-	int i,j,k;
+	int i, j, k;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			k = j*i;
+			k = j * i;
+
 			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
+
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
@@ -22,6 +25,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(k + '0');
 			}
+
 			else if (k >= 10)
 			{
 				_putchar(',');
